1e1e4933e5cfa8cf1aa952a01b857c7c1ca18841
Tatsuhiko Kimita <nannbo@crappo.net>
2021-03-25 09:34:37 +0900

    新規にSpring Bootのスタータープロジェクトを作成した直後の状態をコミット
    by Eclipse(Full Edition)

---
+++ src/main/java/page/clapandwhistle/uam/SampleUserAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package page.clapandwhistle.uam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleUserAccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleUserAccountManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/page/clapandwhistle/uam/SampleUserAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package page.clapandwhistle.uam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleUserAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
