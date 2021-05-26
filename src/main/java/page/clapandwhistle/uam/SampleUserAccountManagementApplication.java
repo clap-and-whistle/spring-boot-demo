76a2973c87b225e3a4c6122cfa9a505ccbfc60ed
Tatsuhiko Kimita <nannbo@crappo.net>
2021-05-26 13:19:19 +0900

    Intellij IDEAのmavenプラグインでpackageビルド(war)に成功した設定

--- src/main/java/page/clapandwhistle/uam/SampleUserAccountManagementApplication.java
+++ src/main/java/page/clapandwhistle/uam/SampleUserAccountManagementApplication.java
@@ -2,9 +2,10 @@ package page.clapandwhistle.uam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SampleUserAccountManagementApplication {
+public class SampleUserAccountManagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleUserAccountManagementApplication.class, args);
