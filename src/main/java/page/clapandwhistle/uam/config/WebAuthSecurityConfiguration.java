0870cd7e1321366e31c8281a46154d42eba15589
Tatsuhiko Kimita <nannbo@crappo.net>
2021-06-22 12:01:19 +0900

    #1: SpringSecurityの設定用クラスの名前が紛らわしかったので変更

--- src/main/java/page/clapandwhistle/uam/config/WebAuthSecurityConfiguration.java
+++ src/main/java/page/clapandwhistle/uam/config/WebAuthSecurityConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class WebBasicAuthSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebAuthSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.csrf().disable();
