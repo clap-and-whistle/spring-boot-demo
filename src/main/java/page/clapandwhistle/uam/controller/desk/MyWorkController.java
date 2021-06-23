afed76af1b214a48b48d00fd2c047ffdd422a342
Tatsuhiko Kimita <nannbo@crappo.net>
2021-06-23 13:54:18 +0900

    #1: 認証後に表示する初期画面や、認証不要で参照可能なコンテンツ用のURLを仮作成した

---
+++ src/main/java/page/clapandwhistle/uam/controller/desk/MyWorkController.java
@@ -0,0 +1,17 @@
+package page.clapandwhistle.uam.controller.desk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyWorkController {
+    public static final String URL_PATH_PREFIX = "desk";
+    public static final String URL_PATH_INDEX = "/index";
+    private final String TEMPLATE_PATH_PREFIX = "desk/my-work/";
+
+    @RequestMapping(URL_PATH_PREFIX + URL_PATH_INDEX)
+    public String index() {
+        return TEMPLATE_PATH_PREFIX + "index";
+    }
+
+}
