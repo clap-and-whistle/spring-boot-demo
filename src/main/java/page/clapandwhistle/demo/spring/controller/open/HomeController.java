252ed9892eb2973889b70383eda7d191b74771ac
Tatsuhiko Kimita <nannbo@crappo.net>
2021-07-08 01:55:28 +0900

    体験用ecパッケージへのリンクをサイトTOPへ追記した

--- src/main/java/page/clapandwhistle/demo/spring/controller/open/HomeController.java
+++ src/main/java/page/clapandwhistle/demo/spring/controller/open/HomeController.java
@@ -1,14 +1,20 @@
 package page.clapandwhistle.demo.spring.controller.open;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import page.clapandwhistle.demo.spring.controller.ec.adm.ItemsMasterController;
 
 @Controller
 public class HomeController {
     public static final String URL_PATH_UNIFIED_ERROR = "/uni-err";
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute(
+                "url_path_ec",
+                ItemsMasterController.URL_PATH_PREFIX + ItemsMasterController.URL_PATH_LIST
+        );
         return "open/home/index";
     }
 
