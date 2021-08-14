ef5e4355784730828adc6529b4ebbf984561b83d
Tatsuhiko Kimita <nannbo@crappo.net>
2021-08-14 10:45:17 +0900

    伴走用ページング実装: リストに表示するプロパティを追加

--- src/main/java/page/clapandwhistle/demo/spring/bizlogic/experimental/BookForSale.java
+++ src/main/java/page/clapandwhistle/demo/spring/bizlogic/experimental/BookForSale.java
@@ -5,10 +5,21 @@ import java.math.BigDecimal;
 public final class BookForSale {
 	private int id;
 	private String name;
+	private String author;
+	private BigDecimal price;
 
 	public BookForSale(int id, String name) {
+		super();
 		this.id = id;
 		this.name = name;
+	}
+
+	public BookForSale(int id, String name, String author, BigDecimal price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.price = price;
 	}
 
 	public int getId() {
@@ -25,6 +36,22 @@ public final class BookForSale {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
 	}
 
 }
