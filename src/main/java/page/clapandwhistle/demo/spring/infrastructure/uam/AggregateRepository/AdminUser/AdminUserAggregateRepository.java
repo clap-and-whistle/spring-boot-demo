cc6269cfe30842abb7f0142f61e83bbe3e7d7b7c
Tatsuhiko Kimita <nannbo@crappo.net>
2021-09-06 21:02:41 +0900

    #16: AdminUserAggregateRepositoryInterfaceの実装クラスがコミット漏れしていたので直した

--- src/main/java/page/clapandwhistle/demo/spring/infrastructure/uam/AggregateRepository/AdminUser/AdminUserAggregateRepository.java
+++ src/main/java/page/clapandwhistle/demo/spring/infrastructure/uam/AggregateRepository/AdminUser/AdminUserAggregateRepository.java
@@ -7,7 +7,11 @@ import page.clapandwhistle.demo.spring.bizlogic.uam.Aggregate.Exception.NotExist
 import page.clapandwhistle.demo.spring.bizlogic.uam.Aggregate.Exception.PasswordIsNotMatchException;
 import page.clapandwhistle.demo.spring.bizlogic.uam.Aggregate.AdminUserAggregateRepositoryInterface;
 import page.clapandwhistle.demo.spring.infrastructure.uam.AggregateRepository.Library.UserAccount.PasswordOperation;
+import page.clapandwhistle.demo.spring.infrastructure.uam.TableModel.AdminAccountBase;
 import page.clapandwhistle.demo.spring.infrastructure.uam.TableModel.AdminAccountBaseRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public final class AdminUserAggregateRepository implements AdminUserAggregateRepositoryInterface {
@@ -21,14 +25,29 @@ public final class AdminUserAggregateRepository implements AdminUserAggregateRep
     }
 
     @Override
-    public long getAdminIdAllowedToLogIn(String email, String password) throws NotExistException, PasswordIsNotMatchException {
-        // TODO: implementation
-        return 0;
+    public long getAdminIdAllowedToLogIn(String email, String password)
+            throws NotExistException, PasswordIsNotMatchException {
+        List<AdminAccountBase> listAdminAccountBase = this.adminBaseDao.findByEmail(email);
+        if (listAdminAccountBase.isEmpty())  throw new NotExistException();
+        if (listAdminAccountBase.size() > 1) throw new RuntimeException("複数ヒットしました");
+
+        AdminAccountBase adminAccountBase = listAdminAccountBase.get(0);
+        if (this.passwordOperator.isPasswordMatch(password, adminAccountBase.getPassword())) {
+            return adminAccountBase.getId();
+        }
+        throw new PasswordIsNotMatchException();
     }
 
     @Override
     public AdminUser findById(long id) {
-        // TODO: implementation
-        return null;
+        Optional<AdminAccountBase> optAdminAccountBase = this.adminBaseDao.findById(id);
+        AdminAccountBase adminAccountBase = optAdminAccountBase.orElse(null);
+        return adminAccountBase == null
+                ? null
+                : AdminUser.buildForFind(
+                        adminAccountBase.getId()
+                        , adminAccountBase.getEmail()
+                        , adminAccountBase.getAccountStatus()
+                    );
     }
 }
